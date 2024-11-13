f87c5faac17a9475ef519cbc2368d031b56654d7
Venera <venii13@icloud.com>
2024-11-13 20:45:25 +0200

    added user-item component

---
+++ Components/demo-app/src/app/user-list/user-item/user-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-item.component.html',
+  styleUrl: './user-item.component.css'
+})
+export class UserItemComponent {
+  user = { name: 'Hardcoded Name', age: -1 };
+
+}
--- Components/demo-app/src/app/user-list/user-list.component.ts
+++ Components/demo-app/src/app/user-list/user-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [UserItemComponent],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
-
+  users = [
+    { name: 'Pesho',  age: 22 },
+    { name: 'Ivan',  age: 33 },
+    { name: 'Mira',  age: 18 },
+    { name: 'Denis',  age: 55 },
+    { name: 'Kircho',  age: 43 },
+  ];
 }
