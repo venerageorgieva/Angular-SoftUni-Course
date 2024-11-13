f95c682a00a3cec48cab4a2dd5f330463900c5e8
Venera <venii13@icloud.com>
2024-11-13 19:04:57 +0200

    html and css in seperate files

--- Components/demo-app/src/app/app.component.ts
+++ Components/demo-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar-component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
