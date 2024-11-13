adfc31e4bd7e82c0138019dcd4f95ad22ecd6356
Venera <venii13@icloud.com>
2024-11-13 19:29:55 +0200

    add footer

--- Components/demo-app/src/app/app.component.ts
+++ Components/demo-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
   imports: [
     RouterOutlet,
     NavigationBarComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
