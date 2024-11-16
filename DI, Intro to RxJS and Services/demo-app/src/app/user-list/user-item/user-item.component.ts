702ef0abfa60f38637eae0739001405828edb253
Venera <venii13@icloud.com>
2024-11-16 15:26:19 +0200

    update Import

--- DI, Intro to RxJS and Services/demo-app/src/app/user-list/user-item/user-item.component.ts
+++ DI, Intro to RxJS and Services/demo-app/src/app/user-list/user-item/user-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { SimpleUser } from '../../types';
-
+import { UserService } from '../user.service';
 @Component({
   selector: 'app-user-item',
   standalone: true,
   imports: [],
   templateUrl: './user-item.component.html',
-  styleUrl: './user-item.component.css'
+  styleUrl: './user-item.component.css',
 })
 export class UserItemComponent {
-@Input('user') user:SimpleUser | null = null;
+  @Input('user') user: SimpleUser | null = null;
+  
+  constructor(private us: UserService) {}
 }
